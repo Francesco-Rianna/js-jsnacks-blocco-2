d13e0dc847f037a586b44d9267dfb0017476bf8f
Francesco-Rianna <maradonino98@hotmail.it>
2024-03-12 19:08:21 +0100

    feat: added snack-1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,3 +1,12 @@
 // - L'utente inserisce un numero nel prompt, se è pari stampa il numero,
 // se è dispari stampa il numero successivo
 
+// chiedo all'utente un numero
+const userNumber = parseInt(prompt('dimmi un numero'))
+
+// se il numero è pari lo stampo, se è dispari stampo il numero successivo 
+if (userNumber % 2 === 0) {
+    console.log(userNumber)
+} else {
+    console.log(userNumber + 1)
+}
